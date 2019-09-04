bcb7aa08348b98bafc92113353df5e1318de8748
zhangtianjie <715751607@qq.com>
2019-09-04 14:30:32 +0800

    fix: 修复了版本获取方式
    原版本获取方式，会导致构建错误。
    修复了如下包：
    - config_file
    - registry

--- config_file/setup.py
+++ config_file/setup.py
@@ -2,9 +2,10 @@
 # Intro: 配置文件模块安装文件
 # Author: Ztj
 # Email: ztj1993@gmail.com
-# Date: 2019-09-02
+# Date: 2019-09-04
 
 import os.path
+import re
 
 from setuptools import setup
 
@@ -12,9 +13,13 @@ f = open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf8')
 readme = f.read()
 f.close()
 
+f = open(os.path.join(os.path.dirname(__file__), 'config_file.py'), encoding='utf8')
+version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
+f.close()
+
 setup(
     name='py-ztj-configfile',
-    version=__import__('config_file').__version__,
+    version=version,
     description='python configuration file loading package',
     long_description=readme,
     long_description_content_type='text/markdown',
