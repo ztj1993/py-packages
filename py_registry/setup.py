66edabb5a76ff0ccac9b1549399f8dc07fe344d2
zhangtianjie <715751607@qq.com>
2019-10-23 15:08:05 +0800

    fix: py_registry/setup.py

--- py_registry/setup.py
+++ py_registry/setup.py
@@ -13,7 +13,7 @@ f = open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf8')
 readme = f.read()
 f.close()
 
-f = open(os.path.join(os.path.dirname(__file__), 'config_file.py'), encoding='utf8')
+f = open(os.path.join(os.path.dirname(__file__), 'registry.py'), encoding='utf8')
 version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
 f.close()
 
