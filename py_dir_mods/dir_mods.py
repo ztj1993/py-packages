ff679acb104724349e25a13c2a07c1370600138a
Ztj <ztj1993@gmail.com>
2019-11-21 18:03:17 +0800

    Update dir_mods.py

--- py_dir_mods/dir_mods.py
+++ py_dir_mods/dir_mods.py
@@ -3,7 +3,7 @@
 # Author: Ztj
 # Email: ztj1993@gmail.com
 # Version: 0.0.1
-# Date: 2019-11-07
+# Date: 2019-11-21
 
 import importlib
 import pkgutil
@@ -18,10 +18,14 @@ class DirMods(object):
 
     def load(self, mod):
         if isinstance(mod, str):
-            mod = importlib.import_module(mod)
+            try:
+                mod = importlib.import_module(mod)
+            except:
+                return self
 
         for _, file, _ in pkgutil.iter_modules(path=mod.__path__, prefix=mod.__name__ + '.'):
             self.mods[file] = importlib.import_module(file)
+        return self
 
     def get(self, name):
         return self.mods.get(name)
@@ -37,4 +41,20 @@ class DirMods(object):
             if not isinstance(mods.get(getattr(mod, var)), dict):
                 mods[getattr(mod, var)] = dict()
             mods[getattr(mod, var)][name] = mod
+        return mods
+
+    def filter(self, var):
+        for name, mod in self.mods.items():
+            if not hasattr(mod, var):
+                self.mods.pop(name)
+            if getattr(mod, var) is False:
+                self.mods.pop(name)
+        return self
+
+    def call(self, var, *args, **kwargs):
+        mods = dict()
+        for name, mod in self.mods.items():
+            if not hasattr(mod, var):
+                continue
+            mods[name] = getattr(mod, var)(*args, **kwargs)
         return mods
